be2b40e6f70d822a828f145f4fb2f79721b9a93c
pissang <bm2736892@gmail.com>
2021-06-04 11:36:38 +0800

    try adding token

--- build/deploy-gh.js
+++ build/deploy-gh.js
@@ -2,6 +2,7 @@ const run = require('@jamesives/github-pages-deploy-action').default
 const path = require('path')
 
 run({
+  token: process.env['GITHUB_TOKEN'],
   branch: 'gh-pages',
   folder: 'dist',
   clean: true,
