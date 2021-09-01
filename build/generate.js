66d7778ccbf11fc1f98b08832e374c765fe24656
pissang <bm2736892@gmail.com>
2021-09-01 19:31:48 +0800

    try to update nav from local built at first

--- build/generate.js
+++ build/generate.js
@@ -2,20 +2,34 @@ const fetch = require('node-fetch')
 const path = require('path')
 const fs = require('fs')
 
+const websitePath = path.resolve(__dirname, '../../echarts-website')
+
 async function updateNav() {
   for (let locale of ['zh', 'en']) {
-    console.log('Fetching...', `http://echarts.apache.org//${locale}/nav.html`)
-    const navContent = await fetch(
-      `http://echarts.apache.org/${locale}/nav.html`
-    ).then(response => response.text())
-
-    fs.writeFileSync(
-      path.join(__dirname, `../components/partials/Navbar/${locale}.vue`),
-      `<template>
-${navContent}
-</template>`,
-      'utf-8'
+    const localPath = `${websitePath}/${locale}/nav.html`
+    const targetPath = path.join(
+      __dirname,
+      `../components/partials/Navbar/${locale}.vue`
     )
+    console.log('Fetching...', localPath)
+    try {
+      fs.copyFileSync(`${websitePath}/${locale}/nav.html`, targetPath)
+    } catch (e) {
+      console.log(
+        'Local file not found. Fetching...',
+        `http://echarts.apache.org/${locale}/nav.html`
+      )
+      const navContent = await fetch(
+        `http://echarts.apache.org/${locale}/nav.html`
+      ).then(response => response.text())
+      fs.writeFileSync(
+        localPath,
+        `<template>
+  ${navContent}
+  </template>`,
+        'utf-8'
+      )
+    }
   }
 }
 
